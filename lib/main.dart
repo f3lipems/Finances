710a02b5e4473202373231c82cba38d8a8f03600
Felipe <felipe@visionar.io>
2024-09-16 11:10:58 -0300

    Add intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(const Finances());
 
@@ -63,7 +64,7 @@ class MyHomePage extends StatelessWidget {
                         ),
                         padding: const EdgeInsets.all(10),
                         child: Text(
-                          tr.value.toString(),
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
@@ -82,7 +83,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: const TextStyle(
                               color: Colors.grey,
                             ),
