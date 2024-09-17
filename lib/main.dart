72eef5f38102256dffbca3dc9c5ac5d5a2dba0c1
Felipe <felipe@visionar.io>
2024-09-17 17:35:05 -0300

    Add bottonSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import './components/transaction_user.dart';
+import 'dart:math';
+
+import './components/transaction_form.dart';
+import './components/transaction_list.dart';
+import '../models/transaction.dart';
 
 void main() => runApp(const Finances());
 
@@ -14,14 +18,51 @@ class Finances extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _trasactions = [
+    Transaction(id: 't1', title: 'Tênis', value: 300.00, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Luz', value: 198.34, date: DateTime.now()),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _trasactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(onSubmit: _addTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+            onPressed: () => _openTransactionFormModal(context),
+            icon: const Icon(Icons.add),
+            color: Colors.white,
+          )
+        ],
         title: const Text(
           'Finanças',
           style: TextStyle(color: Colors.white),
@@ -40,12 +81,12 @@ class MyHomePage extends StatelessWidget {
                 child: Text('Gráfico'),
               ),
             ),
-            const TransactionUser(),
+            TransactionList(transaction: _trasactions),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => _openTransactionFormModal(context),
         child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
